d7b45c4405cd7d062e85d5881608c892b5d07bd2
Benjie Gillam <benjie@jemjie.com>
2017-08-25 09:07:25 +0100

    Add avatarUrl to schema

--- server/userAPIMock.js
+++ server/userAPIMock.js
@@ -3,6 +3,7 @@ const ALL_USERS = [
     id: 1,
     name: 'Benjie',
     website: 'https://graphile.org',
+    avatarUrl: "https://en.gravatar.com/userimage/2548051/640bc01ac2de4f2e778e1120d68ab504.png"
   },
 ];
 
